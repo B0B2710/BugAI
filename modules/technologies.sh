8aca4fa2bd7cdf5117187b436e6de62f93fcf95b
bomba2006 <65975451+bomba2006@users.noreply.github.com>
2023-06-18 17:18:31 +0300

    tech update

--- modules/technologies.sh
+++ modules/technologies.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+if [ -z "$1" ] || [ -z "$2" ]
 then
-    echo "Usage: ./technologies.sh <domain>"
+    echo "Usage: ./technologies.sh <subdomains_file> <output_dir>"
     exit 1
 fi
 
-domain=$1
-outdir=$domain/technologies
+subdomains_file=$1
+output_dir=$2
 
 # Create output directory
-mkdir -p $outdir
+mkdir -p "$output_dir"
 
-# wappalyzer
-echo "[*] Running wappalyzer..."
-docker run --rm -v "$(pwd)":/mnt/app wappalyzer/cli https://$domain > $outdir/wappalyzer.json
+# Iterate over subdomains and run scans
+echo "[*] Scanning subdomains..."
+while read -r subdomain; do
+    echo "[*] Scanning $subdomain..."
+    docker run --rm -v "$(pwd)":/mnt/app wappalyzer/cli -g "https://$subdomain" > "$output_dir/wappalyzer_$subdomain.json"
+    webanalyze -host "$subdomain" -crawl 1 -output "$output_dir/webanalyze_$subdomain.json"
+done < "$subdomains_file"
 
-# webanalyze
-echo "[*] Running webanalyze..."
-webanalyze -host $domain -crawl 1 -output $outdir/webanalyze.json
+# Consolidate results into a single text file
+echo "[*] Consolidating results..."
+cat "$output_dir/wappalyzer.json" > "$output_dir/technologies.txt"
+cat "$output_dir/webanalyze.json" >> "$output_dir/technologies.txt"
+for file in "$output_dir/wappalyzer_"*.json; do
+    cat "$file" >> "$output_dir/technologies.txt"
+    rm "$file"  # Remove individual JSON file
+done
+for file in "$output_dir/webanalyze_"*.json; do
+    cat "$file" >> "$output_dir/technologies.txt"
+    rm "$file"  # Remove individual JSON file
+done
 
-echo "[+] Technology detection complete! Output saved to: $outdir"
-
+echo "[+] Technology detection complete! Consolidated results saved to: $output_dir/technologies.txt"
