0ff2e3f0d9eaa98e28653e67b92af4be81ac315c
BOB <warbobzone@gmail.com>
2023-06-18 16:47:27 +0300

    nmap fix

--- modules/port_scanning.sh
+++ modules/port_scanning.sh
@@ -11,10 +11,26 @@ ip=$1
 output_dir=$2
 scan_mod=$3
 scan_speed=$4
-
+subdomains=$5
+ip_addresses=""
 #nmap
 
+get_subdomain_ips() {
+    local domain="$1"
 
+    # Iterate over subdomains and retrieve IP addresses
+    for subdomain in $subdomains; do
+        ip=$(nslookup -type=A "$subdomain" | awk '/^Address:/{print $2}')
+
+        # Check if IP address already exists in the ip_addresses array
+        if [[ ! " ${ip_addresses[@]} " =~ " ${ip} " ]]; then
+            # Append IP address to the ip_addresses array
+            ip_addresses+=("$ip")
+        fi
+    done
+}
+
+get_subdomain_ips
 nmapScanHandler() {
     
     funcStartTime="start time: $current_datetime"
@@ -26,29 +42,17 @@ nmapScanHandler() {
     elif [[ "$scan_speed" == "f" ]]; then
         speed="T5"
     fi
-
-    if [[ $scan_mod == "a" ]]; then
-
-        
-        sudo nmap -sS -sU "$speed" -A -v -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 --script discovery "$ip" -oN "$output_dir"/portscan.txt
-        sudo nmap -p- -sV -O -sF "$speed" -A "$ip" -oN "$output_dir"/portscan.txt
-
-    fi
-
-    if [[ $scan_mod == "s" || $scan_mod == "S" ]]; then
-        
-
-        sudo nmap -sS -Pn -p- -sV -O "$speed" "$ip" -oN "$output_dir"/portscan.txt
-
-    
-    fi
-
-    if [[ $scan_mod == "d" || $scan_mod == "D" ]]; then
-       
-
-        sudo nmap -sU -sS "$speed" "$ip" -oN "$output_dir"/portscan.txt
-        sudo nmap -O -sF "$speed" "$ip" -oN "$output_dir"/portscan.txt
-    fi
+    for ip in "${ip_addresses[@]}"; do
+        if [[ $scan_mod == "a" ]]; then
+            sudo nmap -sS -sU "$speed" -A -v -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 --script discovery "$ip" -oN "$output_dir"/portscan.txt
+            sudo nmap -p- -sV -O -sF "$speed" -A "$ip" -oN "$output_dir"/portscan.txt
+        elif [[ $scan_mod == "s" || $scan_mod == "S" ]]; then
+            sudo nmap -sS -Pn -p- -sV -O "$speed" "$ip" -oN "$output_dir"/portscan.txt
+        elif [[ $scan_mod == "d" || $scan_mod == "D" ]]; then
+            sudo nmap -sU -sS "$speed" "$ip" -oN "$output_dir"/portscan.txt
+            sudo nmap -O -sF "$speed" "$ip" -oN "$output_dir"/portscan.txt
+        fi
+    done
 
 
 }
