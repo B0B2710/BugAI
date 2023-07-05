3dfb004ad7a52beafe656bf8b4485b69f869b2ad
BOB <warbobzone@gmail.com>
2023-07-05 11:49:01 +0300

    bug

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from bardapi import Bard
 #API_KEY = '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'  # Replace with your actual API key
 
 token = 'XwjlF04MT1t5eGHPdg7-YgaoekM_RD9JpccHjGXhDRJpQZTk4L41lJhI1VQZQFVK925JZA.'
-bardcode = Bard(token=token, run_code=True)
-
+bardcode = Bard(token=token)
+#tools 
 def extract_identifiers(csv_file):
     identifiers = []
     with open(csv_file, 'r') as file:
@@ -31,14 +31,15 @@ def get_text_between_quotes(text):
   end_index = text.find("'''", start_index + 3)
   return text[start_index + 3:end_index]
 
-#args list format [nmap -a -b -c -bbc ,masscan -a -b -c -bbc]
+#args list format [nmap -a -b -c -bbc ,masscan -a -b -c -bbc,]
 def run_scan1(args_as_list):
     args_string = ' '.join(args_as_list)
-    subprocess.call(["bash", "scan1.sh", args_string])
+    scope_string = extract_identifiers(scope_csv_path)
+    subprocess.call(["bash", "scan1.sh",args_string,scope_string])
 
 def get_arg_for_tools(scope_text, rules_text, tools_list):
 
-    arg = bardcode.get_answer(f'based on scope ["{scope_text}"] and rules "{rules_text}" make parms for {tools_list} and answer in this format "nmap (the parms for the command)," instead of saying all the domains u can refer to it as $domains and dont include output parms')['content']
+    arg = bardcode.get_answer(f'based on scope ["{scope_text}"] and rules "{rules_text}" make parms for {tools_list} and answer in this format "nmap (the parms for the command)," instead of saying all the domains u can refer to it as $domain output each tool to $output_dir/tool_name/tool_name.txt')['content']
     return arg
 
 if __name__ == "__main__":
--- modules/subdomain_enumeration.sh
+++ modules/subdomain_enumeration.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
-    echo "Usage: $0 <domain>"
-    exit 1
-fi
+scope_list=$2
+output_file="${1}"
 
-domain="${1}"
-output_file="${2:-$domain/subdomains.txt}"
 
-echo "Starting subdomain enumeration for $domain..."
 
-# First-degree enumeration
-echo "Running subfinder..."
-subfinder -d "$domain" -silent > "$output_file"
-echo "Running assetfinder..."
-assetfinder --subs-only "$domain" >> "$output_file"
-echo "Running Amass..."
-amass enum -silent -d "$domain" >> "$output_file"
-echo "Running massdns..."
-massdns -r /opt/massdns/lists/resolvers.txt -t A -o S -w subdomains_massdns.txt "$output_file"
-awk '{print $1}' subdomains_massdns.txt | sort -u >> "$output_file"
-echo "$domain" >> "$output_file"
-rm subdomains_massdns.txt
+for scope in "${scope_list[@]}"
+do
+    # First-degree enumeration
+    echo "Running subfinder..."
+    subfinder -d "$scope" -silent > "$output_file"
+    echo "Running assetfinder..."
+    assetfinder --subs-only "$scope" >> "$output_file"
+    echo "Running Amass..."
+    amass enum -silent -d "$scope" >> "$output_file"
+    echo "Running massdns..."
+    massdns -r /opt/massdns/lists/resolvers.txt -t A -o S -w subdomains_massdns.txt "$output_file"
+    awk '{print $1}' subdomains_massdns.txt | sort -u >> "$output_file"
+    echo "$scope" >> "$output_file"
+    rm subdomains_massdns.txt
+done
 echo " subdomain_enurmeration done"
+
 
 
 
