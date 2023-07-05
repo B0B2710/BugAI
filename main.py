86120c3229deabc6ae0e68ec494fd559d18cbf6d
bomba2006 <65975451+bomba2006@users.noreply.github.com>
2023-07-05 11:49:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 import pandas as pd
 import csv
 from bardapi import Bard
-
+import re
 #API_KEY = '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'  # Replace with your actual API key
 
 token = 'XwjlF04MT1t5eGHPdg7-YgaoekM_RD9JpccHjGXhDRJpQZTk4L41lJhI1VQZQFVK925JZA.'
-bardcode = Bard(token=token, run_code=True)
+bardcode = Bard(token=token)
 
 def extract_identifiers(csv_file):
     identifiers = []
@@ -24,21 +24,24 @@ def read_file(filename):
         return file.read()
 
 
-def get_text_between_quotes(text):
-    #finish this one and run run_scan1 
-  """Gets the text between two sets of double quotes."""
-  start_index = text.find("'''")
-  end_index = text.find("'''", start_index + 3)
-  return text[start_index + 3:end_index]
+def extract_command(text, tool_name):
+    """Extracts the command for the given tool name and arguments from the given text."""
+    pattern = r"{} (.+?)(?=$|\snmap|\smasscan|\sgobuster)".format(tool_name)
+    matches = re.findall(pattern, text, re.DOTALL)
+    return [match.strip() for match in matches]
+
+
 
 #args list format [nmap -a -b -c -bbc ,masscan -a -b -c -bbc]
-def run_scan1(args_as_list):
-    args_string = ' '.join(args_as_list)
-    subprocess.call(["bash", "scan1.sh", args_string])
+#*def run_scan1(args_as_list):
+#    args_string = ' '.join(args_as_list)
+#    subprocess.call(["bash", "scan1.sh", args_string])
 
 def get_arg_for_tools(scope_text, rules_text, tools_list):
 
-    arg = bardcode.get_answer(f'based on scope ["{scope_text}"] and rules "{rules_text}" make parms for {tools_list} and answer in this format "nmap (the parms for the command)," instead of saying all the domains u can refer to it as $domains and dont include output parms')['content']
+    args = []
+    for tool in tools_list:
+        arg = bardcode.get_answer(f'based on rules "{rules_text}" make parms for {tool} and answer in this format "(the parms for the command)," dont include output parms and give only one command for each tool that is as efficienive as allowed by the rules')
     return arg
 
 if __name__ == "__main__":
@@ -49,5 +52,12 @@ if __name__ == "__main__":
     scope_text = extract_identifiers(scope_csv_path)
     rules_text = read_file(rules_file_path)
 
-    params_list = get_arg_for_tools(scope_text, rules_text, tools_list)
-    print(params_list)
+    arg = get_arg_for_tools(scope_text, rules_text,tools_list)
+    #text = "Sure, here are the parameters for the commands you mentioned, using the scope and rules you provided:**Nmap**Use code with caution. Learn more nmap -sT -p- $domainsnmap -sV -p- $domains nmap -sS -p- $domains nmap --script whois-domain $domains nmap --script smb-enum-domains $domains Code snippet **Masscan** Use code with caution. Learn more masscan -p0-65535 -iL $domains Code snippet **Gobuster** Use code with caution. Learn more gobuster -w /usr/share/wordlists/common.txt -u $domains -t 30 Please note that these are just a few examples of parameters that you can use. You may need to adjust them depending on the specific hosts and services you are scanning. You should also carefully read the rules and policiesof the program you are participating in before running any scans.Here are some additional tips for using these commands:Use the -sT option for a TCP connect scan, which is the most reliable way to scan for open ports.Use the -sV option to perform version detection, which can help you identify the specific software running on a host.Use the -sS option for a SYN scan, which is a less reliable way to scan for open ports but is less likely to be blocked by firewalls.Use the whois-domain script to enumerate domains associated with a host.Use the smb-enum-domains script to enumerate domains associated with a host that is running Microsoft Windows.Use the -w option to specify a wordlist of common usernames and passwords.Use the -u option to specify the URL of the host you want to scan.Use the -t option to specify the number of concurrent threads to use.I hope this helps!"
+    print(arg['code'])
+    print(arg['content'])
+
+
+
+
+    #print(commands)
