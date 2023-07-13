59ce70afd974002aa4dae2ec3a45bb78f41ac601
bomba2006 <65975451+bomba2006@users.noreply.github.com>
2023-07-13 14:41:10 +0300

    Update port_scanning.sh

--- modules/port_scanning.sh
+++ modules/port_scanning.sh
@@ -47,7 +47,7 @@ check_error() {
 
 while IFS= read -r domain; do
     if curl --head --silent --fail "$domain" 2> /dev/null; then
-        if [[ "$(curl -s -o /dev/null -w "%{http_code}" "$domain")" != "302" ]]; then
+        if [[ "$(curl -s -o /dev/null -w "%{http_code}" "$domain")" != "302" || "$(curl -s -o /dev/null -w "%{http_code}" "$domain")" != "301" ]]; then
             echo "[*] Running Nmap on ${domain}"
             sudo nmap $nmap_args "$domain" 2> "error.txt"
             check_error $? "nmap" "$output_dir/nmap.txt" "0"
