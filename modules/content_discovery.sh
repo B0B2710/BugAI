0d944f31a1c93b90c550188b242af5da05bfda6e
bomba2006 <65975451+bomba2006@users.noreply.github.com>
2023-07-13 17:47:16 +0300

    a

--- modules/content_discovery.sh
+++ modules/content_discovery.sh
@@ -12,10 +12,10 @@ IFS='^' read -ra scope_list <<< "$scope_string"
 
 
 #gobuster_args=${args_list[1]}
-feroxbuster_args=${args_list[1]}
-dirsearch_args=${args_list[2]}
-gospider_args=${args_list[3]}
-hakrawler_args=${args_list[4]}
+#feroxbuster_args=${args_list[1]}
+dirsearch_args=${args_list[1]}
+gospider_args=${args_list[2]}
+hakrawler_args=${args_list[3]}
 
 
 
